ead58a0d87ac91dae1fe098874d00a0347fb1056
JuanJosePG <jjpg.code@gmail.com>
2022-04-21 21:20:34 +0200

    Git init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <p v-bind:id="'id' + 2">{{ 2 + 2 }}</p>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+    name: 'HomeView',
+    components: {
+        HelloWorld,
+    },
+    data() {
+        return {
+            nombre: 'pepe',
+        };
+    },
 };
 </script>
