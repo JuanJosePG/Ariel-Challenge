4d063aedfcead1621adc3bde97722a43d82b8c37
JuanJosePG <jjpg.code@gmail.com>
2022-04-22 00:48:14 +0200

    [FIX] Sass: import variables in any vue script

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Bulma from 'bulma';
 
-import './styles/main.scss';
+// import './styles/main.scss';
+
+Vue.use(Bulma);
 
 Vue.config.productionTip = false;
 
