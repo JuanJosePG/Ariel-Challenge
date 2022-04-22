be13db07193c672f6d97b51f761b92e2defb3cf7
JuanJosePG <jjpg.code@gmail.com>
2022-04-22 17:50:06 +0200

    [ADD] Basic layout of todo view

--- src/views/MyTask.vue
+++ src/views/MyTask.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div class="about-page">
+        <h1 class="task-page-title">Mis tareas</h1>
+        <!-- todo items -->
+        <!-- todo input -->
     </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.about-page {
+    height: 100vh;
+    padding: 30px 15px;
+
+    .task-page-title {
+        font-weight: bold;
+        font-size: 20px;
+        text-align: left;
+    }
+}
+</style>
