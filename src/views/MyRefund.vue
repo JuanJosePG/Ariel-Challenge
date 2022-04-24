bff0a0487e5ff6c3b445a9e5adab4e44c4137548
JuanJosePG <jjpg.code@gmail.com>
2022-04-24 21:13:55 +0200

    [ADD] Bonus: add my refund page

---
+++ src/views/MyRefund.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="refund-page">
+        <div v-if="hasRefunds">
+            <!-- Refunds hechas -->
+        </div>
+        <div class="no-refunds" v-else>
+            <h1>No tienes ninguna devolución hecha o pendiente.</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyRefund',
+    data() {
+        return {
+            hasRefunds: false,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.refund-page {
+    height: 100vh;
+    padding: 30px 15px;
+
+    .no-refunds {
+        height: 50vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        h1 {
+            font-size: 20px;
+            font-weight: bold;
+        }
+    }
+}
+</style>
--- src/views/MyTask.vue
+++ src/views/MyTask.vue
@@ -1,24 +1,146 @@
 <template>
-    <div class="about-page">
-        <h1 class="task-page-title">Mis tareas</h1>
+    <div class="task-page">
+        <div class="title-wrapper">
+            <h1 class="task-page-title">Mis tareas</h1>
+            <button
+                class="delete-full-list button is-danger"
+                @click="removeAll()"
+            >
+                Borrar todo
+            </button>
+        </div>
+
         <!-- todo items -->
+        <div v-if="todos.length">
+            <ToDoItem
+                v-for="todo in todos"
+                :key="todo.id"
+                :todo="todo"
+                @remove="removeItem"
+            />
+        </div>
+        <div v-else>
+            <h2>No tienes ninguna tarea creada, empieza ahora!</h2>
+        </div>
         <!-- todo input -->
+        <button
+            class="modal-trigger add-todo-item-button"
+            data-target="modal-todo-item"
+            @click="showModal = true"
+        >
+            Añadir tarea
+        </button>
+        <AddToDoItem
+            :activeModal="showModal"
+            @changeValue="setFalseValue"
+            v-model="newTodoItem"
+            @keydown.enter="addTodo"
+        />
     </div>
 </template>
 
 <script>
-export default {};
+import ToDoItem from '@/components/ToDo/ToDoItem.vue';
+import AddToDoItem from '@/components/ToDo/AddToDoItem.vue';
+
+let idTodoNext = 1;
+
+export default {
+    name: 'MyTask',
+    components: {
+        ToDoItem,
+        AddToDoItem,
+    },
+    data() {
+        return {
+            showModal: false,
+            newTodoItem: {
+                title: '',
+                text: '',
+            },
+            todos: [
+                {
+                    id: idTodoNext++,
+                    title: 'Prueba1',
+                    text: 'Texto1',
+                },
+                {
+                    id: idTodoNext++,
+                    title: 'Prueba2',
+                    text: 'Texto2',
+                },
+                {
+                    id: idTodoNext++,
+                    title: 'Prueba3',
+                    text: 'Texto3',
+                },
+            ],
+        };
+    },
+    mounted() {
+        // axios -> instalar y recoger info
+    },
+    methods: {
+        addToDo() {
+            if (this.newTitleTodo) {
+                this.todos.push({
+                    id: idTodoNext++,
+                    title: this.newTodoItem.title,
+                    text: this.newTodoItem.text,
+                });
+            }
+        },
+        removeItem(idToRemove) {
+            this.todos = this.todos.filter((elem) => {
+                return elem.id !== idToRemove;
+            });
+        },
+        removeAll() {
+            this.todos = [];
+        },
+        setFalseValue(value) {
+            this.showModal = value;
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-.about-page {
+.task-page {
     height: 100vh;
     padding: 30px 15px;
 
-    .task-page-title {
+    .title-wrapper {
+        margin-bottom: 15px;
+        display: flex;
+        justify-content: space-between;
+
+        .task-page-title {
+            font-weight: bold;
+            font-size: 20px;
+            text-align: left;
+        }
+
+        .delete-full-list {
+            border: none;
+            border-radius: 5px;
+            height: 30px;
+
+            &:focus {
+                box-shadow: none;
+            }
+        }
+    }
+
+    .add-todo-item-button {
+        width: 100%;
+        height: 35px;
+        margin-top: 30px;
+        background-color: $main-green;
+        color: white;
         font-weight: bold;
-        font-size: 20px;
-        text-align: left;
+        border: none;
+        border-radius: 5px;
     }
 }
 </style>
